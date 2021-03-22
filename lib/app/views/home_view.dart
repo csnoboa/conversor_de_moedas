a2e1bc9d0a03dbd0b394bc5e379e6a2499def692
csnoboa <caique2noboa@gmail.com>
2021-03-22 09:48:33 -0300

    #31

--- lib/app/views/home_view.dart
+++ lib/app/views/home_view.dart
@@ -26,16 +26,32 @@ class _HomeViewState extends State<HomeView> {
               ),
               Container(height: 50),
               Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Expanded(
                     flex: 1,
-                    child: DropdownButton(
-                      items: [],
+                    child: SizedBox(
+                      height: 55,
+                      child: DropdownButton(
+                        isExpanded: true,
+                        items: [
+                          DropdownMenuItem(child: Text('Real')),
+                          DropdownMenuItem(child: Text('Dolar')),
+                        ],
+                      ),
                     ),
                   ),
                   Container(width: 20),
-                  Expanded(flex: 2, child: TextField()),
+                  Expanded(
+                      flex: 2,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.amber),
+                          ),
+                        ),
+                      )),
                 ],
               ),
               Container(height: 50),
