de3405e5576ad01d95ed03a0d89855eff881aa21
csnoboa <caique2noboa@gmail.com>
2021-03-22 07:04:59 -0300

    realinhamento view

--- lib/app/views/home_view.dart
+++ lib/app/views/home_view.dart
@@ -14,22 +14,37 @@ class _HomeViewState extends State<HomeView> {
       body: SizedBox(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: Column(
-          children: [
-            Image.asset('assets/logo.png'),
-            Row(
-              children: [
-                DropdownButton(
-                  items: [],
-                ),
-                Container(width: 150, child: TextField()),
-              ],
-            ),
-            ElevatedButton(
-              child: Text('CONVERTER'),
-              onPressed: null,
-            ),
-          ],
+        child: Padding(
+          padding:
+              const EdgeInsets.only(top: 50.0, left: 20, right: 20, bottom: 20),
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/logo.png',
+                width: 200,
+                height: 200,
+              ),
+              Container(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: DropdownButton(
+                      items: [],
+                    ),
+                  ),
+                  Container(width: 20),
+                  Expanded(flex: 2, child: TextField()),
+                ],
+              ),
+              Container(height: 50),
+              ElevatedButton(
+                child: Text('CONVERTER'),
+                onPressed: null,
+              ),
+            ],
+          ),
         ),
       ),
     );
